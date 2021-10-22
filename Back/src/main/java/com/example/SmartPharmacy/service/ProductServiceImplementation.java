f6dd9ec7e039286df3dfb635b9b99190e647257f
Sandun Herath <sandunherath124@gmail.com>
2021-10-22 06:42:36 +0530

    service implementations done

--- Back/src/main/java/com/example/SmartPharmacy/service/ProductServiceImplementation.java
+++ Back/src/main/java/com/example/SmartPharmacy/service/ProductServiceImplementation.java
@@ -1,4 +1,74 @@
 package com.example.SmartPharmacy.service;
 
-public class ProductServiceImplementation {
+import com.example.SmartPharmacy.Repository.ProductRepository;
+import com.example.SmartPharmacy.model.Products;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class ProductServiceImplementation implements ProductService{
+    private final ProductRepository productRepository;
+    @Override
+    public Products SaveProduct(Products product) {
+        log.info("Saving new Product to database",product.getName());
+
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Products updateProductImage(String image, Long id) {
+        Products existing= productRepository.findById(id).get();
+        if(existing !=null){
+            existing.setImage(image);
+
+        }
+        return productRepository.save(existing);
+    }
+
+    @Override
+    public Products getSingleProduct(Long id) {
+        log.info("Fetching product {}",id);
+        return productRepository.findById(id).get();
+    }
+
+    @Override
+    public Products updateProduct(Products product, Long id) {
+        Products existing= productRepository.findById(id).get();
+        if(existing !=null){
+            existing.setName(product.getName());
+            existing.setPrice(product.getPrice());
+            existing.setDescription(product.getDescription());
+            existing.setImage(product.getImage());
+            existing.setCategory(product.getCategory());
+
+            return productRepository.save(existing);
+        }else {
+            log.error("Product not found in the database");
+            return null;
+        }
+    }
+
+
+
+    @Override
+    public void deleteProduct(Long id) {
+        Products product= productRepository.findById(id).get();
+        if(product !=null){
+            productRepository.deleteById(id);
+        }
+
+    }
+
+    @Override
+    public List<Products> getProducts() {
+        log.info("Fetching All Products");
+        return productRepository.findAll();
+    }
 }
--- Back/src/main/java/com/example/SmartPharmacy/service/UserServiceImplementation.java
+++ Back/src/main/java/com/example/SmartPharmacy/service/UserServiceImplementation.java
@@ -1,4 +1,56 @@
 package com.example.SmartPharmacy.service;
 
-public class UserServiceImplementation {
+import com.example.SmartPharmacy.Repository.RoleRepository;
+import com.example.SmartPharmacy.Repository.UserRepository;
+import com.example.SmartPharmacy.model.Role;
+import com.example.SmartPharmacy.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImplementation implements UserService{
+    private final UserRepository userRepo;
+    private final RoleRepository roleRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public User saveUser(User user) {
+        log.info("Saving new user to database",user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        log.info("Saving new user to database",role.getName());
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role{} to user {}",roleName,username);
+        User user=userRepo.findByUsername(username);
+        Role role= roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        log.info("Fetching user {}",username);
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        log.info("Fetching All Users");
+        return userRepo.findAll();
+    }
 }
