017612007bfaa67e5215c2c1846482a32ace0705
Sandun Herath <sandunherath124@gmail.com>
2021-10-22 06:04:38 +0530

    repo completed

--- Back/src/main/java/com/example/SmartPharmacy/Repository/UserRepository.java
+++ Back/src/main/java/com/example/SmartPharmacy/Repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.example.SmartPharmacy.Repository;
 
-public interface UserRepository {
+import com.example.SmartPharmacy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
 }
