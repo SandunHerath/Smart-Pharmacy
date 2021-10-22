cd1430fee874cdbc5719ba5b6df453d8cefd2ffa
Sandun Herath <sandunherath124@gmail.com>
2021-10-22 05:48:17 +0530

    initial commit

---
+++ Back/src/test/java/com/example/SmartPharmacy/SmartPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartPharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
